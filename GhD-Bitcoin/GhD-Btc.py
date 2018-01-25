0bb7f8289648ecbd68e1acb9f753470876768edf
Alberto_Reyes_F <albertoreyes@protonmail.com>
2018-01-25 20:42:55 +0100

    Migrado a Python3. Eliminadas dependencias innecesarios

--- GhD-Bitcoin/GhD-Btc.py
+++ GhD-Bitcoin/GhD-Btc.py
@@ -1,21 +1,8 @@
-# -*- coding: utf-8 -*-
-
-import sys
-import os
 import time
-import sqlite3
 import requests
 from BDBtc import *
-from coinbase.wallet.client import Client
-
 
 crearBDBtc()
-#
-# [apiKey,apiSecret]=['Vioq9ZeOIyvt98Zw','68yCkxPl5EAHXhIMGDDCdHXIfwXrXWrO']
-# API_VERSION = '2016-02-18'
-# client = Client(apiKey,apiSecret,API_VERSION)
-# currencyPair='ETH-EUR'
-
 
 while 1:
     rBuy = requests.get("https://api.coinbase.com/v2/prices/ETH-EUR/buy").json()
@@ -28,7 +15,7 @@ while 1:
     tempo=time.time()
 
     actualizarPrecios(prezoCompra, prezoVenta, prezoSpot,tempo)
-    print 'Prezo compra: ' + str(obterPrezoCompra(tempo))
-    print 'Prezo venta: ' + str(obterPrezoVenta(tempo))
-    print 'Prezo Spot: ' + str(obterPrezoSpot(tempo))
+    print('Prezo compra: ' + str(obterPrezoCompra(tempo)))
+    print('Prezo venta: ' + str(obterPrezoVenta(tempo)))
+    print('Prezo Spot: ' + str(obterPrezoSpot(tempo)))
     time.sleep(5)
